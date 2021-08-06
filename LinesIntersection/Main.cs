fdd3d6f6cfde7033a49cef2b362515450ed4ba5d
Danil <leonardo147wot@gmail.com>
2021-08-06 16:19:21 +0300

    LogOutput

--- LinesIntersection/Main.cs
+++ LinesIntersection/Main.cs
@@ -10,11 +10,18 @@ using Autodesk.AutoCAD.Geometry;
 using Exception = Autodesk.AutoCAD.Runtime.Exception;
 using System.Windows;
 using Application = Autodesk.AutoCAD.ApplicationServices.Application;
+using DS_SystemTools;
 
 namespace LinesIntersection
 {
     class Main
     {
+        List<int> X_Coords = new List<int>();
+        List<int> Y_Coords = new List<int>();
+
+        //Get current date and time    
+        readonly string CurDateTime = DateTime.Now.ToString("yyMMdd_HHmmss");
+
         public void StartTransaction()
         {
             // Get the current document and database
@@ -40,6 +47,8 @@ namespace LinesIntersection
                 // Save the new object to the database
                 acTrans.Commit();
             }
+
+            LogOutput();
         }
 
         void CreateLines(BlockTableRecord acBlkTblRec, Transaction acTrans)
@@ -51,8 +60,19 @@ namespace LinesIntersection
                 int i;
                 for (i = 0; i < 10; i++)
                 {
-                    Point3d Point1 = new Point3d(rnd.Next(1, 13), rnd.Next(1, 13), 0);
-                    Point3d Point2 = new Point3d(rnd.Next(1, 13), rnd.Next(1, 13), 0);
+                    int x1 = rnd.Next(1, 13);
+                    int x2 = rnd.Next(1, 13);
+                    int y1 = rnd.Next(1, 13);
+                    int y2 = rnd.Next(1, 13);
+
+                    Point3d Point1 = new Point3d(x1, y1, 0);
+                    Point3d Point2 = new Point3d(x2, y2, 0);
+
+                    //add X and Y coordinates to lists
+                    X_Coords.Add(x1);
+                    X_Coords.Add(x2);
+                    Y_Coords.Add(y1);
+                    Y_Coords.Add(y2);
 
                     // Create a line that starts at Point1 and ends at Point2
                     Line acLine = new Line(Point1, Point2);
@@ -70,7 +90,28 @@ namespace LinesIntersection
             }
         }
 
-      
+        void LogOutput()
+        {
+            DS_Tools dS_Tools = new DS_Tools
+            {
+                DS_LogOutputPath = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\Desktop\Logs\"),
+                DS_LogName = CurDateTime + "_Log.txt"
+            };
+            dS_Tools.DS_StreamWriter("Lines coordinates start and end: \n");
 
+            int i;
+            int number = 0;
+            for (i = 0; i < X_Coords.Count - 1; i+=2)
+            {
+                number++;
+                
+                dS_Tools.DS_StreamWriter("Line " + number.ToString() + ": " + 
+                    X_Coords[i].ToString() + ","+ Y_Coords[i].ToString() + "; " + 
+                    X_Coords[i + 1].ToString() + "," + Y_Coords[i +1].ToString());
+                
+
+        }
+            dS_Tools.DS_FileExistMessage();
+        }
     }
 }
