c4c2a3e7dbd99c48013a0f7eb986070ac2beec3d
Danil <leonardo147wot@gmail.com>
2021-08-09 14:37:55 +0300

    Mongo added

---
+++ LayersSync/Models/MyObject.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LayersConstructor
+{
+    // by implementing the INotifyPropertyChanged, changes to properties
+    // will update the listbox on-the-fly
+    public class MyObject : INotifyPropertyChanged
+    {
+        private string _name;
+
+
+        // a property.
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+      public class  LayerField: INotifyPropertyChanged
+    {
+        private string _code;
+        private string _decription;
+
+
+        // a property.
+        public string Code
+        {
+            get { return _code; }
+            set
+            {
+                if (_code != value)
+                {
+                    _code = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Code)));
+                }
+            }
+        }
+
+
+        // a property.
+        public string Description
+        {
+            get { return _decription; }
+            set
+            {
+                if (_decription != value)
+                {
+                    _decription = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Description)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ LayersSync/NamesCollection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+
+namespace LayersSync
+{
+    public class NamesCollection : ObservableCollection<MyObject>
+    {       
+        public void Add(string name)
+        {
+            this.Add(new MyObject { Name = name });
+        }
+    }
+
+    public class LayersFieldsCollection : ObservableCollection<LayerField>
+    {      
+        public void Add(string code, string decription)
+        {
+            this.Add(new LayerField { Code = code, Description = decription });
+        }
+    }
+}
