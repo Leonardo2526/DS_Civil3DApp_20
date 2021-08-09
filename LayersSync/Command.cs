5e85c74bce9359ed95ba19a36919b7e171d8bc42
Danil <leonardo147wot@gmail.com>
2021-08-09 12:55:02 +0300

    One layer creartion added

---
+++ LayersSync/Command.cs
@@ -0,0 +1,39 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LayersSync
+{
+    public class Commands : IExtensionApplication
+    {
+        // функция инициализации (выполняется при загрузке плагина)
+        public void Initialize()
+        {
+            MessageBox.Show("Hello!");
+        }
+
+        // функция, выполняемая при выгрузке плагина
+        public void Terminate()
+        {
+            MessageBox.Show("Goodbye!");
+        }
+
+        // эта функция будет вызываться при выполнении в AutoCAD команды «TestCommand»
+        [CommandMethod("DS_CreateLayer")]
+        public void MyCommand()
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        [CommandMethod("ShowForm")]
+        public void ShowFormCommand()
+        {
+            
+        }
+    }
+}
---
+++ LayersSync/Main.cs
@@ -0,0 +1,53 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Colors;
+using Autodesk.AutoCAD.DatabaseServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayersSync
+{
+    class Main
+    {
+        public void CreateAndAssignALayer()
+        {
+            // Get the current document and database
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+            DocumentLock acLckDoc = acDoc.LockDocument();
+
+            // Start a transaction
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Open the Layer table for read
+                LayerTable acLyrTbl;
+                acLyrTbl = acTrans.GetObject(acCurDb.LayerTableId,
+                                             OpenMode.ForRead) as LayerTable;
+
+                string sLayerName = "Center";
+
+                if (acLyrTbl.Has(sLayerName) == false)
+                {
+                    LayerTableRecord acLyrTblRec = new LayerTableRecord();
+
+                    // Assign the layer the ACI color 1 and a name
+                    acLyrTblRec.Color = Color.FromColorIndex(ColorMethod.ByAci, 1);
+                    acLyrTblRec.Name = sLayerName;
+
+                    // Upgrade the Layer table for write
+                    acLyrTbl.UpgradeOpen();
+
+                    // Append the new layer to the Layer table and the transaction
+                    acLyrTbl.Add(acLyrTblRec);
+                    acTrans.AddNewlyCreatedDBObject(acLyrTblRec, true);
+                }
+
+
+                // Save the changes and dispose of the transaction
+                acTrans.Commit();
+            }
+        }
+    }
+}
