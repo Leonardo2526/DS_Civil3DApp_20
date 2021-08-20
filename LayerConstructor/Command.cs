08b065bae7b18a96d07b0641eeab483bb3aa3cce
Leonardo2526 <leonardo147wot@gmail.com>
2021-08-20 14:30:10 +0300

    start project

---
+++ LayerConstructor/Command.cs
@@ -0,0 +1,39 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LayerConstructor
+{
+    public class Commands : IExtensionApplication
+    {
+        // функция инициализации (выполняется при загрузке плагина)
+        public void Initialize()
+        {
+            MessageBox.Show("Hello!");
+        }
+
+        // функция, выполняемая при выгрузке плагина
+        public void Terminate()
+        {
+            MessageBox.Show("Goodbye!");
+        }
+
+        // эта функция будет вызываться при выполнении в AutoCAD команды «TestCommand»
+        [CommandMethod("TestCommand")]
+        public void MyCommand()
+        {
+            MessageBox.Show("Habr!");
+        }
+
+        [CommandMethod("ShowForm")]
+        public void ShowFormCommand()
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
